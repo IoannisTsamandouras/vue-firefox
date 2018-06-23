a32c2e4bde7bf43ed766a2cdeda069069fafb0f9
IoannisTsamandouras <ioannis.tsamandouras@gmail.com>
2018-06-23 21:46:57 +0300

    Initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyDOY_QRtdTVug9B7T4TEXpIw58etJXWiJ4",
+    authDomain: "vue-js-4f977.firebaseapp.com",
+    databaseURL: "https://vue-js-4f977.firebaseio.com",
+    projectId: "vue-js-4f977",
+    storageBucket: "vue-js-4f977.appspot.com",
+    messagingSenderId: "732738763915"
+}
---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from '@/components/Dashboard'
+import EditEmployee from '@/components/EditEmployee'
+import ViewEmployee from '@/components/ViewEmployee'
+import NewEmployee from '@/components/NewEmployee'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/new',
+      name: 'new-employee',
+      component: NewEmployee
+    },
+    {
+      path: '/:employee_id',
+      name: 'view-employee',
+      component: ViewEmployee
+    },
+    {
+      path: '/edit/:employee_id',
+      name: 'edit-employee',
+      component: EditEmployee
+    }
+  ]
+})
